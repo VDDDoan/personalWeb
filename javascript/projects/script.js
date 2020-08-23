890c7b857ea634677140a541e9d6b6e304c0e984
David Doan <vandoandinh@gmail.com>
2020-08-23 01:30:10 -0700

    header complete

---
+++ javascript/projects/script.js
@@ -0,0 +1,24 @@
+//For the hamburger menu, adds responsive to the class 
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav');
+    const navLinks = document.querySelectorAll('.nav li');
+    
+    burger.addEventListener('click', () => {
+        //toggles nav
+        nav.classList.toggle('nav-active');
+        //Animates links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation ='';
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
