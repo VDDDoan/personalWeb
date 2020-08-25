9efe4b2c2c4a04a6c8b86eefa0bbfb426f458726
David Doan <vandoandinh@gmail.com>
2020-08-24 17:52:49 -0700

    added animation and more css

--- javascript/home/script.js
+++ javascript/home/script.js
@@ -22,3 +22,21 @@ const navSlide = () => {
 }
 
 navSlide();
+
+document.addEventListener('DOMContentLoaded', () => {
+    function animateSgv (id, delay, delayIncrement){
+        const logo = document.getElementById(id);
+        const logoPaths = document.querySelectorAll(`#${id} path`);
+        delay = delay;
+        for(let i = 0; i < logoPaths.length;i++){
+            //console.log(logoPaths[i].getTotalLength());
+            logoPaths[i].style.strokeDasharray  = logoPaths[i].getTotalLength();
+            logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
+            logoPaths[i].style.animation = `line-anim 2s ease forwards ${delay}s`;
+            delay+=delayIncrement;
+            console.log(delay)
+        }
+        logo.style.animation = `fill 0.5s ease forwards ${delay}s`;
+    }
+    animateSgv('name-svg', 1, 0.3)
+}, false);
